bd5f54e89de6b40c27dc31f2a1f358d5b27dbb88
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2022-02-25 13:30:26 -0500

    Add files via upload

---
+++ src/AssignmentRunner.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class AssignmentRunner {
+
+    public static void main(String[] args) {
+
+        // Start here
+
+    }
+
+    public static double average(ArrayList<Assignment> assignments) {
+       
+       
+    }
+}
